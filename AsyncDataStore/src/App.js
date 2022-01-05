751541f64a3a802e4328a0be77cfaa8cccf332b2
Rana Sakshi <ranasakshi.imscit17@gmail.com>
2022-01-05 21:43:23 +0530

    modified async storage project

--- AsyncDataStore/src/App.js
+++ AsyncDataStore/src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import Login from './screens/Login';
 
-const App=() => {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>
-        11
-      </Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
- 
-});
-
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#0080ff'
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: 'bold'
+          }
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 export default App;
