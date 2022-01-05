736b7bdaa07e0fb6a09a957da6f9afee8811988a
Rana Sakshi <ranasakshi.imscit17@gmail.com>
2022-01-05 19:04:16 +0530

    new projects

---
+++ AsyncDataStore/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App=() => {
+  return (
+    <View>
+      <Text>
+        11
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
---
+++ SQLiteDataStore/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App= () => {
+ 
+  return (
+    <View><Text>11</Text></View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
